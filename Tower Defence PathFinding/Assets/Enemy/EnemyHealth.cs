3bf45c7ce08424c1f4b14e4351158bd04273d271
JonoAlejandro <jonathan.moran1999@gmail.com>
2021-09-01 23:28:55 +1000

    Added

---
+++ Tower Defence PathFinding/Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+    [Tooltip("Adds Amount to MaxHitPoints when enemy dies.")]
+
+    [SerializeField] int difficultyRamp = 1;
+    int currentHitPoints = 0;
+
+    Enemy enemy;
+
+    private void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    //Called when Object gets enabled.
+    void OnEnable()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if (currentHitPoints <= 0)
+        {
+            gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
+            enemy.RewardGold();
+        }
+    }
+}
