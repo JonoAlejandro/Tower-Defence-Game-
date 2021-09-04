88d30ef3b6e3d7ca1230c5cbb4ad041195187cc6
Reede-P <89636015+Reede-P@users.noreply.github.com>
2021-09-04 14:51:28 +1000

    Menu Implementation
    Functioning menu with volume adjusters too, looks like shit atm tho, will work on the theming after the game is more fleshed out

--- Tower Defence PathFinding/Assets/Scripts/Menu.cs
+++ Tower Defence PathFinding/Assets/Scripts/Menu.cs
@@ -5,16 +5,40 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Audio;
 public class Menu : MonoBehaviour
-{
+{ 
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public AudioMixer mixer;
+
     public void playButton()
     {
         int levelOne = SceneManager.GetActiveScene().buildIndex +1;
         SceneManager.LoadScene(levelOne);
     }
+    public void QuitGame()
+    {
+        Debug.Log("Game Quitting");
+        Application.Quit();
+    }
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        mixer.SetFloat("Music Volume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("Music Volume", sliderValue);
+    }
+    public void SetSFXLevel(float sliderValue)
+    {
+        mixer.SetFloat("SFX Volume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SFX Volume", sliderValue);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        musicSlider.value = PlayerPrefs.GetFloat("Music Volume", 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFX Volume", 1f);
+        Cursor.visible = true;
     }
 
 
